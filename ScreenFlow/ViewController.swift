6cb1bcf25b0d76af52277c79d667710b9b4a1530
Sanjay Noronha <funza@sanjays-macbook-air.home>
2016-02-26 10:40:16 +0100

    Initial Version

---
+++ ScreenFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenFlow
+//
+//  Created by Sanjay Noronha on 2016/02/18.
+//  Copyright © 2016 funza Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
